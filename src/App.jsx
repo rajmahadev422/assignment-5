08e3bb7a4c173865acc7ea5e4856eb658d329a18
rajmahadev422 <rajmahadev422@gmail.com>
2025-10-24 11:58:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,17 @@ import useFetch from "./useFetch";
 function App() {
   const url = 'https://api.escuelajs.co/api/v1/products';
   
-  const { data, loading, error } = useFetch(url);
+  const { d, l, e } = useFetch(url);
 
-  if (loading) return <Loader />;
+  if (l) return <Loader />;
 
-  if(error) return <p>{error}</p>
+  if(e) return <p>{error}</p>
   
   return (
     <>
     <Header />
       <div className="min-h-screen bg-gray-50 flex flex-wrap gap-6 justify-center items-center p-6">
-        {data.length == 0 ? <p className="text-4xl">No data</p> : data.map((item, i) => (
+        {d.length == 0 ? <p className="text-4xl">No data</p> : d.map((item, i) => (
           <div
             key={i}
             className="flex-1 min-w-[300px]"
