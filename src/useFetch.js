d2dda6788b573c915b53d5fe7ea6b8324b705396
rajmahadev422 <rajmahadev422@gmail.com>
2025-10-24 11:57:04 +0530

    Update useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -2,23 +2,23 @@ import { useState, useEffect } from "react";
 
 function useFetch(url) {
   const [urlData, setUrlData] = useState({
-    data: [],
-    loading: false,
-    error: null,
+    d: [],
+    l: false,
+    e: null,
   });
 
   useEffect(() => {
     const fetchData = async () => {
-      setUrlData({ data: null, loading: true, error: null });
+      setUrlData({ d: null, l: true, e: null });
 
       try {
         const response = await fetch(url);
 
         const products = await response.json();
 
-        setUrlData({ data: [...products], loading: false, error: null });
+        setUrlData({ d: [...products], l: false, e: null });
       } catch (err) {
-        setUrlData({ data: null, loading: false, error: err.message });
+        setUrlData({ d: null, l: false, e: err.message });
       }
     };
     fetchData();
@@ -28,3 +28,4 @@ function useFetch(url) {
 }
 
 export default useFetch;
+
